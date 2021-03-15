1be478495f91ef260fbbf0d3e045441031091188
emmy <emmy66418@gmail.com>
2021-03-15 22:37:08 +0200

    Add dot and remove -1 at the of length

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 def bubble_sort(array)
   for i in 0..array.length 
-    for j in i+1..array.length-1  
+    for j in i+1...array.length 
       if(array[i] > array[j])  
       array[i], array[j] = array[j], array[i] 
       end
@@ -10,4 +10,4 @@ def bubble_sort(array)
   return array
 end
 
-p bubble_sort([1,5,7,100,5,8,2,9])
+p bubble_sort([1,5,7,100,5,8,2,9,90])
