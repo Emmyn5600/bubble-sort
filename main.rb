be6f65848ff41f0572d76c845333764495b319f4
tiagorahal <rahal.aires@gmail.com>
2021-03-15 17:23:06 -0300

    bubble sort finish

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 def bubble_sort(array)
   for i in 0..array.length 
-    for j in i+1..array.size-1  
+    for j in i+1..array.length-1  
       if(array[i] > array[j])  
       array[i], array[j] = array[j], array[i] 
       end
