12b80e2a4a6309741240d195d91fd80c45aa05a5
tiagorahal <rahal.aires@gmail.com>
2021-03-15 17:15:51 -0300

    linters test

---
+++ main.rb
@@ -0,0 +1,11 @@
+
+def bubble_sort(array)
+  for i in 0..array.length 
+    for j in i+1..array.size-1  
+      if(array[i] > array[j])  
+      array[i], array[j] = array[j], array[i] 
+      end
+    end
+  end
+  return array
+end
