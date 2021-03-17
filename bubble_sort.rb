0ad32f0e8abe7a67bbe79d829978bf7a6ec77a5b
tiagorahal <rahal.aires@gmail.com>
2021-03-17 19:21:15 -0300

    Second part finish, linter check

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,6 +11,19 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort([1, 5, 7, 100, 5, 8, 2, 9, 90])
-
 # bubble_sort_by
+
+def bubble_sort_by(string)
+  string.length.times do
+    change = 0
+    (string.length - 1).times do |i|
+      if yield(string[i], string[i + 1]).positive?
+        string[i], string[i + 1] = string[i + 1], string[i]
+        change += 1
+      end
+    end
+    break string if change.zero?
+  end
+end
+
+bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
